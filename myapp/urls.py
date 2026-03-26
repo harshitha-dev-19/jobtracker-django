9d7f57182767a8d5be50934c9d03be2843109291
harshi <harshithapn19@gmail.com>
2026-03-26 17:43:03 +0530

    revert changes made for testing

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import home
+from . import views
+
 urlpatterns = [
-    path('',home),
- ]
+    path('', views.job_list, name='job_list'),
+    path('add/', views.add_job, name='add_job'),
+    path('update/<int:id>/', views.update_job, name='update_job'),
+    path('delete/<int:id>/', views.delete_job, name='delete_job'),
+
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+]
