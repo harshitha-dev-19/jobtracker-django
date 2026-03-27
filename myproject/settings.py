f49a049649b0a1ae70fc526d73b90dd09f4dc52a
harshi <harshithapn19@gmail.com>
2026-03-27 12:01:35 +0530

    changes in settings to deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q(ia^csw_nrveo#x+xxksrj103rk!3gn+grog!(+&*vzag=15h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
